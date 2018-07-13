79c8f61487ba1038a32be8a35198ef2f597031eb
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2018-07-13 15:48:21 +0100

    add score_file arg

--- src/scripts/score.py
+++ src/scripts/score.py
@@ -3,13 +3,14 @@ import json
 import sys
 from fever.scorer import fever_score
 from prettytable import PrettyTable
-from analyse import analyse
+from analyse import print_confusion_mat, save_wrong_instances, save_simple_result
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--predicted_labels",type=str)
 
 parser.add_argument("--predicted_evidence",type=str)
 parser.add_argument("--actual",type=str)
+parser.add_argument("--score_file",type=str)
 
 args = parser.parse_args()
 
@@ -35,7 +36,9 @@ for ev,label in zip(predicted_evidence,predicted_labels):
     predictions.append({"predicted_evidence":ev,"predicted_label":label})
 
 score,acc,precision,recall,f1 = fever_score(predictions,actual)
-analyse(predictions, actual)
+print_confusion_mat(predictions, actual)
+save_simple_result(args.score_file, score, acc, precision, recall)
+# save_wrong_instances(args.actual, args.predicted_labels, args.predicted_evidence, args.score_file)
 
 tab = PrettyTable()
 tab.field_names = ["FEVER Score", "Label Accuracy", "Evidence Precision", "Evidence Recall", "Evidence F1"]
